9715a542f00386be34494fceb16c96380fc44c40
saranshbasu01 <saranshbasu@gmail.com>
2025-03-05 08:38:46 +0545

    Add files via upload

--- translation.cpp
+++ translation.cpp
@@ -17,23 +17,18 @@ int main()
 {
 	int gd = DETECT, gm;
 	initgraph(&gd, &gm, (char *)"");
+	char c;
 	int x1,y1,x2,y2,sx,sy,tx,ty,a,choice = 0;
-	while(choice!=4)
-	{
-
-		// cleardevice();
+	do{
+	
 		cout<<"enter your choice: "<<endl;
 		cout<<"1.translation: "<<endl;
 		cout<<"2.scaling: "<<endl;
-		cout<<"3.rotation: "<<endl;
-		cout<<"4.exit"<<endl;
 		cout<<"enter your choice: "<<endl;
 		cin>>choice;
 		switch(choice)
 		{
 			case 1:
-			
-
 				cleardevice();
 				cout<<"enter the points: "<<endl;
 				cin>>x1>>y1>>x2>>y2;
@@ -45,43 +40,29 @@ int main()
 				setcolor(CYAN);
 				line(x1+319+tx,239-y1+ty,x2+319+tx,239-y2+ty);
 				getch();
-			
 			break;
 			case 2:
-					// cleardevice();
-					cout<<"scaling"<<endl;
-					 cout<<"enter the points: "<<endl;
-					 cin>>x1>>y1>>x2>>y2;
-					cout<<"enter sx and sy: "<<endl;
-					cin>>sx>>sy;
-					line(x1+319,239-y1,x2+319,239-y2);
-					cplus();
-					setcolor(CYAN);
-					line(x1*sx+319,239-y1*sy,x2*sx+319,239-y2*sy);
-					getch();
-				
+				int a,b,c,d,e,f,sx,sy;
+				cout<<"enter values\n";
+				cin>>a>>b>>c>>d>>e>>f>>sx>>sy;
+				setcolor(WHITE);
+				line(a,b,c,d);
+				line(c,d,e,f);
+				line(a,b,e,f);
+				cplus();
+				setcolor(CYAN);
+				line(a*sx-125,b*sy-150,c*sy-125,d*sx-150);
+				line(c*sx-125,d*sy-150,e*sy-125,f*sx-150);
+				line(a*sx-125,b*sy-150,e*sy-125,f*sx-150);
+				getch();
 			break;
-			case 3:				
-					// cleardevice();
-					cout<<"rotation"<<endl;
-					// cout<<"enter the points: "<<endl;
-					// cin>>x1>>y1>>x2>>y2;
-					cout<<"enter the angle to be rotated: "<<endl;
-					cin>>a;
-					a=(a*3.14)/180;
-					line(x1+319,239-y1,x2+319,239-y2);
-					cplus();
-					setcolor(CYAN);
-					line((x1*cos(a)-x1*sin(a))+319,239-(y1*sin(a)+y1*cos(a)),(x2*cos(a)-x2*sin(a))+319,239-(y2*sin(a)+y2*cos(a)));
-					getch();
-				break;
-			case 4:
-			
-				exit(0);
-			
+		default:
 			break;
 		}
-	}
+		cout<<"continue";
+		cin>>c;
+	}while(c=='y');
 	getch();
+	closegraph();
 
 }
